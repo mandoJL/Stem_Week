bc6c5e760363eec0eddee85d4b37e7dc959000c4
Julio A. López A <79216361+mandoJL@users.noreply.github.com>
2021-04-22 16:01:58 -0500

    Add files via upload

---
+++ Condicional if-elif-else.py
@@ -0,0 +1,7 @@
+﻿resp=input("¿Quieres ser mi novia?(Escribe Si o No)")
+if resp == "Si":
+    print("No crei llegar tan lejos :) lo bueno que me bañe ayer")
+elif resp=="No":
+    print("Super F y se escucha de fondo la canción fake de titanic :(")
+else:
+    print("De lo que te pierdes mi ciela")
